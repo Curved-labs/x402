e271c6d103477cc61c80597c80f1811c51e8ea40
Crifdotfun <257376032+Crifdotfun@users.noreply.github.com>
2026-01-21 11:30:00 +0900

    feat: complete decoder registry dispatch

--- src/decoder/registry.rs
+++ src/decoder/registry.rs
@@ -26,3 +26,31 @@ impl DecoderRegistry {
         let token22 = Arc::new(Token2022Decoder::new());
         decoders.insert(token22.program_id(), token22);
 
+        // Multisig governance
+        let squads = Arc::new(SquadsDecoder::new());
+        decoders.insert(squads.program_id(), squads);
+
+        // DeFi protocols (Anchor programs behind the generic decoder)
+        let jup = Arc::new(jupiter::decoder());
+        decoders.insert(jup.program_id(), jup);
+
+        let drift = Arc::new(drift_v2::decoder());
+        decoders.insert(drift.program_id(), drift);
+
+        let kamino_d = Arc::new(kamino::decoder());
+        decoders.insert(kamino_d.program_id(), kamino_d);
+
+        let marginfi_d = Arc::new(marginfi::decoder());
+        decoders.insert(marginfi_d.program_id(), marginfi_d);
+
+        Self { decoders }
+    }
+
+    pub fn get(&self, program_id: &Pubkey) -> Option<&Arc<dyn ProgramDecoder>> {
+        self.decoders.get(program_id)
+    }
+
+    pub fn registered_program_ids(&self) -> Vec<Pubkey> {
+        self.decoders.keys().copied().collect()
+    }
+}
